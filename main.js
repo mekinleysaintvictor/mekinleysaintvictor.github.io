9fe70e4c2fcfe2033ad711d9b8fc3862399184fa
unknown <mekinleydsaintvictor@outlook.com>
2021-08-24 17:31:15 -0400

    Skeleton framework of my portfolio website.

---
+++ main.js
@@ -0,0 +1,8 @@
+//used to include full screen background
+var ready = (callback) => {
+    if (document.readyState != "loading") callback();
+    else document.addEventListener("DOMContentLoaded", callback);
+}
+ready(() => {
+    document.querySelector(".header").style.height = window.innerHeight + "px";
+})
